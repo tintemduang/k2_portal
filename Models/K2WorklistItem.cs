2b153d88f455f60c7b681ece0bb2064c6fbe1e86
Tin <tin.temduang@gmail.com>
2026-03-11 10:42:25 +0700

    Edit worklist

--- Models/K2WorklistItem.cs
+++ Models/K2WorklistItem.cs
@@ -3,9 +3,9 @@ namespace K2Portal.Models
     public class K2WorklistItem
     {
         public string? Folio { get; set; }
-        public string? ProcessName { get; set; }
+        public string? WorkflowDisplayName  { get; set; }
         public string? ActivityName { get; set; }
-        public string? StartDate { get; set; }
+        public DateTime? StartDate { get; set; }
         public string? SerialNumber { get; set; }
     }
 }
